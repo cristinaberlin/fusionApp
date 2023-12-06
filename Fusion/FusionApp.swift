b9c0ba5c693874227a6a7c5a1cc51d8b88a3ab6b
cristinaberlin <cristina980@icloud.com>
2023-12-06 17:23:11 +0000

    Initial Commit

---
+++ Fusion/FusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FusionApp.swift
+//  Fusion
+//
+//  Created by Cristina Berlinschi on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct FusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
